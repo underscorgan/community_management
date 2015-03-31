6af8e447331b9333e367575ee73af31c612287a6
Morgan Haskel <morgan@puppetlabs.com>
2015-03-31 17:30:06 -0400

    pull out before/after stuff

--- pull_requests.rb
+++ pull_requests.rb
@@ -5,11 +5,15 @@ require_relative 'octokit_utils'
 
 options = {}
 parser = OptionParser.new do |opts|
-  opts.banner = 'Usage: release_planning.rb [options]'
+  opts.banner = 'Usage: pull_requests.rb [options]'
 
-  opts.on('-a', '--all', 'ALL THE THINGSS') { options[:all] = true }
+  opts.on('-a', '--after DAYS', 'Pull requests that have been updated since DAYS days ago.') { |v| options[:after] = v.to_i }
+  opts.on('-b', '--before DAYS', 'Pull requests that have not been updated since DAYS days ago.') { |v| options[:before] = v.to_i }
+  opts.on('-c', '--count', 'Only print the count of pull requests.') { options[:count] = true }
+  opts.on('-e', '--show-empty', 'List repos with no pull requests') { options[:empty] = true }
   opts.on('-n', '--namespace NAME', 'GitHub namespace. Required.') { |v| options[:namespace] = v }
   opts.on('-r', '--repo-regex REGEX', 'Repository regex') { |v| options[:repo_regex] = v }
+  opts.on('-s', '--sort', 'Sort output based on number of pull requests') { options[:sort] = true }
   opts.on('-t', '--oauth-token TOKEN', 'OAuth token. Required.') { |v| options[:oauth] = v }
   opts.on('-v', '--verbose', 'More output') { options[:verbose] = true }
 end
@@ -33,18 +37,46 @@ repos = util.list_repos(options[:namespace], options)
 repo_data = []
 
 repos.each do |repo|
-  begin
+  #begin
     pulls = util.fetch_pull_requests("#{options[:namespace]}/#{repo}")
 
-    repo_data << { 'repo' => "#{options[:namespace]}/#{repo}", 'pulls' => pulls }
+    if options[:before] and options[:after]
+      opts = { :pulls => pulls }
+      start_time = Time.now - options[:before]
+      end_time = Time.now - options[:after]
+      pulls = util.pulls_in_range(start_time, end_time, opts)
+    elsif options[:before]
+      opts = { :pulls => pulls }
+      start_time = Time.now - options[:before]
+      pulls = util.pulls_older_than(start_time, opts)
+    elsif options[:after]
+      opts = { :pulls => pulls }
+      end_time = Time.now - options[:after]
+      pulls = util.pulls_newer_than(end_time, opts)
+    end
+
+    if not options[:empty] and pulls.empty?
+      next
+    end
+
+    if options[:count]
+      repo_data << { 'repo' => "#{options[:namespace]}/#{repo}", 'pulls' => nil, 'pull_count' => pulls.length }
+    else
+      repo_data << { 'repo' => "#{options[:namespace]}/#{repo}", 'pulls' => pulls, 'pull_count' => pulls.length }
+    end
   rescue
     puts "Unable to fetch pull requests for #{options[:namespace]}/#{repo}" if options[:verbose]
   end
 end
 
-if options[:all]
-  repo_data.each do |entry|
-    puts "=== #{entry['repo']} ==="
+if options[:sort]
+  repo_data.sort_by! { |x| -x['pull_count'] }
+end
+
+repo_data.each do |entry|
+  puts "=== #{entry['repo']} ==="
+  puts "  #{entry['pull_count']} open pull requests"
+  unless options[:count]
     entry['pulls'].each do |pull|
       puts "  #{pull[:html_url]}"
     end
