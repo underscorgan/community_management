bb5666b56fee0d08cbf0eb7c4bd5aa476396bdb6
Morgan Haskel <morgan@puppetlabs.com>
2015-03-31 17:31:47 -0400

    Add counts, sort, and ability to not print empty
    Still todo: before, after support

--- pull_requests.rb
+++ pull_requests.rb
@@ -7,8 +7,6 @@ options = {}
 parser = OptionParser.new do |opts|
   opts.banner = 'Usage: pull_requests.rb [options]'
 
-  opts.on('-a', '--after DAYS', 'Pull requests that have been updated since DAYS days ago.') { |v| options[:after] = v.to_i }
-  opts.on('-b', '--before DAYS', 'Pull requests that have not been updated since DAYS days ago.') { |v| options[:before] = v.to_i }
   opts.on('-c', '--count', 'Only print the count of pull requests.') { options[:count] = true }
   opts.on('-e', '--show-empty', 'List repos with no pull requests') { options[:empty] = true }
   opts.on('-n', '--namespace NAME', 'GitHub namespace. Required.') { |v| options[:namespace] = v }
@@ -37,23 +35,8 @@ repos = util.list_repos(options[:namespace], options)
 repo_data = []
 
 repos.each do |repo|
-  #begin
+  begin
     pulls = util.fetch_pull_requests("#{options[:namespace]}/#{repo}")
-
-    if options[:before] and options[:after]
-      opts = { :pulls => pulls }
-      start_time = Time.now - options[:before]
-      end_time = Time.now - options[:after]
-      pulls = util.pulls_in_range(start_time, end_time, opts)
-    elsif options[:before]
-      opts = { :pulls => pulls }
-      start_time = Time.now - options[:before]
-      pulls = util.pulls_older_than(start_time, opts)
-    elsif options[:after]
-      opts = { :pulls => pulls }
-      end_time = Time.now - options[:after]
-      pulls = util.pulls_newer_than(end_time, opts)
-    end
 
     if not options[:empty] and pulls.empty?
       next
