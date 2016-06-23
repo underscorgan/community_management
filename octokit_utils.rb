87527964f68929fe69180e1f93b35bf73699b028
tphoney <tp@puppet.com>
2016-06-23 11:58:13 +0100

    remove windows supported modules

--- octokit_utils.rb
+++ octokit_utils.rb
@@ -6,7 +6,6 @@ class OctokitUtils
   attr_accessor :client
 
   SUPPORTED_MODULES = [
-    'acl',
     'apache',
     'apt',
     'aws',
@@ -26,17 +25,12 @@ class OctokitUtils
     'netscaler',
     'ntp',
     'postgresql',
-    'powershell',
     'puppet_authorization',
-    'reboot',
-    'registry',
-    'sqlserver',
     'stdlib',
     'tagmail',
     'tomcat',
     'vcsrepo',
     'vsphere',
-    'wsus_client',
   ]
 
   SUPPORTED_MODULES_REGEX = "^(puppetlabs-(#{SUPPORTED_MODULES.join('|')})|modulesync_configs)$"
